90d4312e75237fc27d51301cc1187d057a5c931e
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-09 05:54:39 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if(katz_deli.length == 0)
+    puts "The line is currently empty."
+  else
+      new_array = []
+      katz_deli.each do |i|
+         #punctuation = if(i == katz_deli.length - 1)? " " : "."
+         new_array.push("#{i+ 1}. #{katz_deli[i]}")
+      end
+    puts "The line is currently:#{new_array}"
+  end
+end
