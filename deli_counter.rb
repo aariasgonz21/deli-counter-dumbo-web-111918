499f7dbe7716c0d875519cb4814ad465067df07c
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-09 08:05:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,15 @@
 # Write your code here.
+#require "pry"
 
-katz_deli = []
+deli = []
 
-def line(katz_deli)
-  if(katz_deli.length == 0)
+def line(deli)
+  if(deli.length == 0)
     puts "The line is currently empty."
   else
-      new_array = []
-      katz_deli.each do |i|
-         #punctuation = if(i == katz_deli.length - 1)? " " : "."
-         new_array.push("#{i+ 1}. #{katz_deli[i]}")
-      end
-    puts "The line is currently:#{new_array}"
+      list = ""
+      deli.each_with_index{|name, num|
+        
+      }
   end
 end
